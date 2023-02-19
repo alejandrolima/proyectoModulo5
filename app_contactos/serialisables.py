292a1a38bc9827bb1004adf1a59ce65d75fbb4f9
Alejandro Lima Maldonado <allima69@gmail.com>
2023-02-19 17:50:22 -0400

    Proyecto Moulo 5

---
+++ app_contactos/serialisables.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import serializers
+
+from app_contactos.models import Categorias
+
+class CategoriaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Categorias
+        fields = ['id', 'nombre', 'descripcion', 'fecha_alta', 'fecha_actualizacion']
+
+class CategoriasViewSet(viewsets.ModelViewSet):
+
+  queryset = Categorias.objects.all()
+  serializer_class = CategoriaSerializer
