954c3c758a8ac7e406202ac1644a288e0d9c24e8
Alejandro Lima Maldonado <allima69@gmail.com>
2023-03-26 19:44:10 -0400

    Modificacion, Dockerizacion

--- proyectoa_agenda/settings.py
+++ proyectoa_agenda/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4v@%1j8l^*cmo^=h%buu+^m_8&ka(q^ix=!v%-i_b*nwih%+zl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0:8080','192.168.0.11:8080','*']
 
 
 # Application definition
